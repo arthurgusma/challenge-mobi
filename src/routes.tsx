84a1e3357a28ba53e515682b88aa6ab9ebf7094d
gusmadev <arthur.cabj@hotmail.com>
2025-03-28 18:11:56 -0300

    refactor: routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,17 +1,31 @@
-import { createBrowserRouter } from "react-router";
-import HomePage from "./pages/home";
+import { createBrowserRouter, Navigate } from "react-router";
+import HomePage from "./pages/Home";
 import { NotFound } from "./404";
-import ResultPage from "./pages/result";
-
+import ResultPage from "./pages/Result";
+import Error from "./error";
+import { ResultLayout } from "./pages/_layouts/result";
 
 export const router = createBrowserRouter([
-    { path: '/', 
-        children: [
-        { path: '/', element: <HomePage /> },
-        { path: '/resultado', element: <ResultPage /> }
-    ]},
+    { path: '/',
+        children: [{ 
+            path: '/',
+            element: <Navigate to="/pesquisa" replace />,
+        },
+        {
+            path: '/pesquisa', element: <HomePage />
+        }
+    ],
+     },
+     
+    { path: '/resultado', element: <ResultLayout />,
+        children: [{
+            path: '/resultado', 
+            element: <ResultPage /> 
+        }],
+    },
     {
         path: '*',
-        element: <NotFound />
+        element: <NotFound />,
+        errorElement: <Error />
     }
 ])
